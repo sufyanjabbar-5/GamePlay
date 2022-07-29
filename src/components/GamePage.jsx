42c6741eac42de5df40e27706135eb971a84f089
sufyanjabbar-5 <sufyanjabbar5@gmail.com>
2022-07-29 11:10:21 +0500

    j

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -4,11 +4,8 @@ import { Stack, Text } from '@chakra-ui/react';
 const GamePage = () => {
   return (
     <Stack height={'800px'} alignItems={'center'} bg={'#161940'} w={'100%'}>
-      <Text color={'white'}>spinner</Text>
-      <div class="lds-dual-ring">
-       {/* <div style={{color:'white',position:'absolute',top:'100%',left:"100%"}}>21</div> */}
-       {/* <div style={{border:'1px solid white',position:'absolute',top:'100%',left:"100%"}}></div> */}
-      </div>
+      <div class="lds-dual-ring"></div>
+   
     </Stack>
   );
 };
