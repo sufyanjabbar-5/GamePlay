15e9dad30bc196e18bff5b629e8dc74cccf8f060
sufyanjabbar-5 <sufyanjabbar5@gmail.com>
2022-08-01 21:30:27 +0500

    spinner

--- src/App.js
+++ src/App.js
@@ -10,10 +10,14 @@ import {
   theme,
 } from '@chakra-ui/react';
 import GamePage from './components/GamePage'
+import CountdownTimer from './components/CountdownTimer';
 function App() {
   return (
     <ChakraProvider theme={theme}>
+      <CountdownTimer/>
       <GamePage/>
+      
+
     </ChakraProvider>
   );
 }
