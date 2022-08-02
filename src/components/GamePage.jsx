90cbe54bf547f394ecf9cf9fb9515214adefd7e8
humajamil <92516436+humajamil@users.noreply.github.com>
2022-08-02 15:35:33 +0500

    'update'

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './GamePlay.css';
+
 import {
   CircularProgress,
   CircularProgressLabel,
@@ -7,12 +8,6 @@ import {
   Text,
 } from '@chakra-ui/react';
 const GamePage = () => {
-  const [number, setnumber] = useState();
- 
-  setInterval( function () {
-    let number1 = Math.floor(Math.random() * 21 + 1);
-    setnumber(number1);
-  }, 6000);
   return (
     <Stack
       height={'100vh'}
@@ -22,24 +17,32 @@ const GamePage = () => {
       spacing={'4'}
       px={{ base: '4', md: '8', lg: '16' }}
     >
-         <Stack
-          border={'1px solid #FF00FF'}
-          boxShadow="#772138 2px 2px 4px 0px, #772138 1px 2px 21px 8px"
-          borderRadius={'lg'}
-          bgColor="#1A1F26"
-          w={'60%'}
-          px="4"
-          py={'1'}
-          textAlign="center"
-          spacing={'7'}
-          direction='row'
-          alignItems={'end'}
-        >
-          <Text color={'#00F0FF'} fontSize='1.5em'>10</Text>
-          <Text color={'#00F0FF'} fontSize='1.5em'>3</Text>
-          <Text color={'#00F0FF'} fontSize='1.5em'>17</Text>
-          <Text color={'#20FC9D'} fontSize='1.7em' fontWeight={'600'}>9</Text>
-        </Stack>
+      <Stack
+        border={'1px solid #FF00FF'}
+        boxShadow="#772138 2px 2px 4px 0px, #772138 1px 2px 21px 8px"
+        borderRadius={'lg'}
+        bgColor="#1A1F26"
+        w={'60%'}
+        px="4"
+        py={'1'}
+        textAlign="center"
+        spacing={'7'}
+        direction="row"
+        alignItems={'end'}
+      >
+        <Text color={'#00F0FF'} fontSize="1.5em">
+          10
+        </Text>
+        <Text color={'#00F0FF'} fontSize="1.5em">
+          3
+        </Text>
+        <Text color={'#00F0FF'} fontSize="1.5em">
+          17
+        </Text>
+        <Text color={'#20FC9D'} fontSize="1.7em" fontWeight={'600'}>
+          9
+        </Text>
+      </Stack>
       <CircularProgress
         trackColor="inherit"
         capIsRound
@@ -83,7 +86,7 @@ const GamePage = () => {
                 p={'12'}
                 align={'center'}
                 justify={'center'}
-                spacing='-2'
+                spacing="-2"
               >
                 <Text
                   flex={'1'}
@@ -93,10 +96,11 @@ const GamePage = () => {
                   }
                   backgroundClip={'text'}
                   style={{ WebkitTextStroke: '1px #20fc9d' }}
-                  fontWeight='600'
+                  fontWeight="600"
+             
                 >
-                  17
                 </Text>
+                <p class="odometer" id="odometer">0</p>
                 <Text flex={'2'} color={'#FCA120'} px={'4'} fontSize={'md'}>
                   Will the next number be Higher or Lower
                 </Text>
