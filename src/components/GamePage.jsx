0ee510acfd69a6aed5d6444d8b9797827890fda2
asheikh184 <asheikh184@gmail.com>
2022-08-02 15:38:21 +0500

    t

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -1,28 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './GamePlay.css';
-import { CircularProgress, CircularProgressLabel, IconButton, Stack, Text } from '@chakra-ui/react';
+import { Box, CircularProgress, CircularProgressLabel, IconButton, Stack, Text } from '@chakra-ui/react';
 import { BsFillTriangleFill } from 'react-icons/bs'
 
-
-function useInterval(callback, delay) {
-  const savedCallback = useRef();
-
-  // Remember the latest callback.
-  useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
-
-  // Set up the interval.
-  useEffect(() => {
-    let id = setInterval(() => {
-      savedCallback.current();
-    }, delay);
-    return () => clearInterval(id);
-  }, [delay]);
-}
-
 const GamePage = () => {
-  const [counter, setCounter] = useState(100);
   const [number, setnumber] = useState();
 
   setInterval(
@@ -31,13 +12,6 @@ const GamePage = () => {
       setnumber(number1);
     }
   )
-  useInterval(
-    () => {
-      while (counter <= 100) {
-        setCounter(counter - 5);
-      }
-    }, 1000
-  );
 
 
   return (
@@ -61,7 +35,7 @@ const GamePage = () => {
           <Text color={'#00F0FF'} fontSize='1.5em'>17</Text>
           <Text color={'#20FC9D'} fontSize='1.7em' fontWeight={'600'}>9</Text>
         </Stack>
-        <CircularProgress trackColor='inherit' capIsRound thickness={'6px'} className='circlular progress' value={counter} size={'500px'} color={'#20fc94'} alignItems={'center'} justifyContent={'center'} display={'flex'}>
+        <CircularProgress trackColor='inherit' capIsRound thickness={'6px'} className='circlular progress' value={80} size={'500px'} color={'#20fc94'} alignItems={'center'} justifyContent={'center'} display={'flex'}>
           <CircularProgressLabel className='circlular progress lable' borderRadius={'full'} alignItems={'center'} justifyContent={'center'} display={'flex'}>
             <Stack border={'5px solid #20FC9D'} borderRadius={'full'} h={'385px'} w={'385px'} align={'center'} justify={'center'}>
               <Stack border={'5px solid #13cee6'} borderRadius={'full'} h={'350px'} w={'350px'} boxShadow={'0 0 25px #13cee6'} align={'center'} justify={'center'}>
@@ -74,8 +48,6 @@ const GamePage = () => {
           </CircularProgressLabel>
         </CircularProgress>
         <Stack direction={'row'} justify={'space-between'} color={'white'}>
-          <IconButton _hover={{}} _focus={{}} _active={{}} variant={'ghost'} icon={<BsFillTriangleFill size={'4em'} style={{ background: 'radial-gradient(circle, rgba(201,18,191,1) 0%, rgba(134,40,206,1) 51%)', backgroundClip: 'border-box' }} />} w={'fit-content'} h={'fit-content'} />
-          <IconButton _hover={{}} _focus={{}} _active={{}} variant={'ghost'} icon={<BsFillTriangleFill size={'4em'} style={{ transform: 'rotate(180deg)' }} />} w={'fit-content'} h={'fit-content'} />
         </Stack>
       </Stack>
     </Stack>
